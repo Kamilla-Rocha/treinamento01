7bf83fcf8af796017ffb5417d7d27844634de8f7
kamilla <kamillasr15@gmail.com>
2025-03-18 00:02:36 -0300

    pegando informação do formulario

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -11,7 +11,37 @@ form!:FormGroup
 
 
   ngOnInit(): void {
-    this.form= new FormGroup({})
+    this.form= new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      idade: new FormControl('', [Validators.required, Validators.min(1)]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  
+    this.form.get('nome')?.valueChanges.subscribe(valor => {
+      console.log('Nome atualizado:', valor);
+    });
+  
+    this.form.get('idade')?.valueChanges.subscribe(valor => {
+      console.log('Idade atualizada:', valor);
+    });
+  
+    this.form.get('email')?.valueChanges.subscribe(valor => {
+      console.log('Email atualizado:', valor);
+    });
+  //   this.form.patchValue({
+  //     nome: usuario.nome,
+  //     idade: usuario.idade,
+  //     email: usuario.email
+    
+  // }
   }
-
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      alert('Formulário enviado com sucesso!');
+    } else {
+      alert('Por favor, preencha todos os campos corretamente.');
+    }
+  }
 }
+
--- src/app/interfaces/informacao.interface.ts
+++ src/app/interfaces/informacao.interface.ts
@@ -2,6 +2,6 @@ export interface Usuario {
     id: 1;
     nome: 'Kamilla';
     email: 'kamilla@email.com';
-    idade?: number; // Propriedade opcional
+    idade: number; 
   }
   
